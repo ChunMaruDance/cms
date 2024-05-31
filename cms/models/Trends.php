a540cf556adf4dd593be3fa45a342a5650f8bd5f
MVY <MVY@d.c>
2024-05-31 18:17:18 +0300

    delete extra logic in Trends model

--- cms/models/Trends.php
+++ cms/models/Trends.php
@@ -18,45 +18,15 @@ class Trends extends Model {
 
     public function __construct(){}
 
-    public static function findByCategories($category){
-        $tableName = self::$table;
-         return Core::get()->db->select(
-            "{$tableName} a",
-            'a.*', 
-            [
-                'JOIN category_accessory ca ON ca.accessory_id = a.id',
-                'JOIN categories c ON c.id = ca.category_id'
-            ], 
-            [
-                'c.title'=> $category
-            ]
-        );
+
+    public static function getAllWithEncodeImage(){
+        $items = self::getAll();
+        foreach ($items as $item) {
+            $item->image = 'data:image/png;base64,' . base64_encode($item->image);   
+         }
+         return $items;
     }
 
-    public function save(){
-        if(!isset($this->fieldsArray['id'])){
-            Core::get()->db->insert(static::$table, $this->fieldsArray);
-        }else{
-
-            $id = $this->fieldsArray['id'];
-            unset($this->fieldsArray['id']);
-            Core::get()->db->update(static::$table, $this->fieldsArray,[
-                static::$primaryKey => $id
-            ]);
-        }
-      
-    }
-
-    public static function findByIdWithEncodeImage($accessoryId){
-        $accessory = self::findById($accessoryId);
-        
-        if($accessory == null){
-            return null;
-        }   
-
-        $accessory->image = 'data:image/png;base64,' . base64_encode($accessory->image);   
-        return $accessory;
-    }
 
 
 }
